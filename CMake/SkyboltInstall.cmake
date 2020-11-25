bef915bde29881521a33c61c4d1f3c9b715d691b
Matthew Reid <matthewreid87@gmaill.com>
2020-11-26 01:13:52 +1100

    Install plugins to bin/plugins

--- CMake/SkyboltInstall.cmake
+++ CMake/SkyboltInstall.cmake
@@ -6,3 +6,12 @@ macro(skybolt_install target)
 		RUNTIME DESTINATION bin
 	)
 endmacro()
+
+macro(skybolt_plugin_install target)
+	install(TARGETS ${target}
+		EXPORT SkyboltTargets
+		LIBRARY DESTINATION lib
+		ARCHIVE DESTINATION lib
+		RUNTIME DESTINATION bin/plugins
+	)
+endmacro()
