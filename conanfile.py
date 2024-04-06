142cf66546da50af564035a3c1fdc4ce641df8e7
Matthew Reid <matthewreid87@gmaill.com>
2024-04-06 21:20:56 +1100

    Upped version to 1.6

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class SkyboltConan(ConanFile):
     name = "skybolt"
-    version = "1.5.0"
+    version = "1.6.0"
     settings = "os", "compiler", "arch", "build_type"
     options = {
         "enable_bullet": [True, False],
@@ -35,12 +35,14 @@ class SkyboltConan(ConanFile):
     no_copy_source = True
 
     requires = [
-        "boost/1.75.0@_/_",
-        "catch2/2.13.8@_/_",
-        "cpp-httplib/0.10.1@_/_",
-        "earcut/2.2.3@_/_",
-        "glm/0.9.9.8@_/_",
-        "nlohmann_json/3.10.5@_/_",
+        "boost/1.75.0@_/_#989077de56cb85b727be210b5827d52f",
+        "catch2/2.13.8@_/_#ac821c6881627aece6c7063bd5aa73ea",
+        "cpp-httplib/0.10.1@_/_#5078af8ecb0001ebdc8c799d38ac9b16",
+        "earcut/2.2.3@_/_#7c612e8a3119c4cbf446b7096ecf2831",
+		"expat/2.5.0@_/_#91e43e4544923e4c934bfad1fa4306f9", # Indirect dependency to resolve version conflict between readosm and fontconfig
+        "glm/0.9.9.8@_/_#550ca1927ce2617e53b4514cff326923",
+        "nlohmann_json/3.10.5@_/_#1ebcf334c3f52d96e057d5aba398c491",
+        "zlib/1.2.13@_/_#97d5730b529b4224045fe7090592d4c1" # Indirect dependency to resolve version conflict between libpng and boost
 	]
 
     def include_package(self, name, version, subfolder=None):
@@ -64,7 +66,7 @@ class SkyboltConan(ConanFile):
         self.include_package("openscenegraph-mr", "3.7.0", "all")
 
         if self.options.enable_bullet:
-            self.requires("bullet3/3.22a@_/_")
+            self.requires("bullet3/3.22a@_/_#29b44d5d03e941af7f47285c379fef16")
 
         if self.options.enable_cigi:
             self.include_package("cigicl", "4.0.6a")
@@ -74,15 +76,15 @@ class SkyboltConan(ConanFile):
             self.include_package("xsimd", "7.4.10")
 
         if self.options.enable_map_features_converter:
-            self.requires("readosm/1.1.0a@_/_")
+            self.requires("readosm/1.1.0a@_/_#23e27a65a8846ce66e7f151d95c9229f")
 
         if self.options.enable_python:
-            self.requires("pybind11/2.9.1@_/_")
+            self.requires("pybind11/2.9.1@_/_#017b6606f856caa02c085b034720791e")
             
         if self.options.enable_sprocket:
-            self.requires("ois/1.5@_/_")
-            self.requires("qt/5.15.11@_/_")
-            self.requires("qwt/6.2.0@_/_")
+            self.requires("ois/1.5@_/_#9b0cf516bb04e6393b37e088d6f6d83e")
+            self.requires("qt/5.15.11@_/_#64fc18b0c5ab189f347993a9853144ad")
+            self.requires("qwt/6.2.0@_/_#8534a295b1e8c7ca6915f52601fb56b7")
             self.include_package("toolwindowmanager", "1.0.0")
 
     def build(self):
