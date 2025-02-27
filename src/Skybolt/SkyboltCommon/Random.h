d41f0024680313ed9dff36f49453bb0122dc2aa5
Matthew Reid <matthewreid87@gmaill.com>
2025-02-27 16:01:34 +1100

    Fixded bug where random number generator seed had no effect

--- src/Skybolt/SkyboltCommon/Random.h
+++ src/Skybolt/SkyboltCommon/Random.h
@@ -1,43 +1,49 @@
 /* Copyright Matthew Reid
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #pragma once
 
-#include "boost/random.hpp"
+#include <random>
 
 namespace skybolt {
 
 class Random
 {
-	typedef boost::minstd_rand base_generator_type;
 public:
-	Random(int seed) :
-		generator(seed),
-		rnd(generator, boost::uniform_int<>(0, RAND_MAX))
+	Random(std::uint32_t seed)
 	{
+		// Seed with a sequence of prime numbers to achieve good initial state. 
+		std::vector<std::uint32_t> seedData = {seed, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157};
+		std::seed_seq seedSeq(seedData.begin(), seedData.end());
+		generator.seed(seedSeq);
+
+		// Warm up the random number generator by discarding samples to improve randomness.
+		generator.discard(10000);
 	}
 
 	int getInt(int minValue, int maxValue)
 	{
-		return rnd() % (1 + maxValue - minValue) + minValue;
+		std::uniform_int_distribution<int> dist(minValue, maxValue);
+		return dist(generator);
 	}
 
 	float unitRand()
 	{
-		return (float)rnd()/RAND_MAX;
+		std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+		return dist(generator);
 	}
 
 	float rangedRand(float minValue, float maxValue)
 	{
-		return minValue + unitRand() * (maxValue - minValue);
+		std::uniform_real_distribution<float> dist(minValue, maxValue);
+		return dist(generator);
 	}
 
 private:
-	base_generator_type generator;
-	boost::variate_generator<base_generator_type&, boost::uniform_int<> > rnd;
+	std::mt19937 generator;
 };
 
 } // namespace skybolt
